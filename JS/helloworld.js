823a9d698efe37b3cbb216ba1ebe731cc6eba127
Mohammad Zeeshan <zeeshanm1997@gmail.com>
2020-10-01 02:32:41 +0530

    Update helloworld.js

--- JS/helloworld.js
+++ JS/helloworld.js
@@ -1,4 +1,14 @@
 /* 
+    
     Task : Write a Hello World program in JavaScript.
-    Author : 
+    Author : Zest
 */
+
+var http = require("http");
+
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
